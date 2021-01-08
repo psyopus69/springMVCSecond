2cb25db658e6b23321c4297377261abbf57cfe22
psyopus69 <GitBest666>
2021-01-08 22:23:11 +0300

    Realize CRUD. First part. Get(READ) add mock database PersonDAO + Person configure Spring by java class use annotations:     @Controller     @Component - for add auto Bean     @RequestMapping - parent URL     @Autowired     @GetMapping - children(own) URL
    In thymeleaf
        th:each
        "person : ${people}"
    
        th:href
        @{*/{id}(id = $*})}
    
        also th:text="${}"

---
+++ src/main/java/ru/dp29/springMVCSecond/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ru.dp29.springMVCSecond.dao;
+
+import org.springframework.stereotype.Component;
+import ru.dp29.springMVCSecond.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "John"));
+        people.add(new Person(++PEOPLE_COUNT, "Bill"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
